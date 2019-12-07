07dff54d652fe76bc7cf1da4d23e78ca37196142
John Paul Wile <botsone@gmail.com>
2019-12-07 15:41:07 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Amoleddit-Twitter-Bot - A bot that scrapes http://reddit.com/r/amoledbackgrounds for images and posts them to twitter.
---
+++ app.js
@@ -0,0 +1,88 @@
+const request = require('request')
+const fs = require('fs')
+const Snooper = require('reddit-snooper')
+const Twit = require('twit')
+const chokidar = require('chokidar')
+
+require('dotenv').config()
+
+const snooper = new Snooper({
+    username: process.env.REDDIT_USER_NAME,
+    password: process.env.REDDIT_PASSWORD,
+    app_id: process.env.REDDIT_APP_ID,
+    api_secret: process.env.REDDIT_API_SECRET,
+    user_agent: process.env.REDDIT_USER_AGENT
+})
+
+const twit = new Twit({
+    consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+    access_token:         process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    timeout_ms:           60*1000,
+    strictSSL:            true
+})
+  
+
+// Set global vars
+let author
+let title
+let url
+
+// Event listener to watch for new posts on subreddit and download them
+snooper.watcher.getPostWatcher('Amoledbackgrounds')
+.on('post', function(newPost) {
+
+	let urlMatch = newPost.data.url.match('\.([a-zA-Z]+$)')
+	// Check to make sure the post is not stickied, that it's an image post, and that it contains a link
+	if (!newPost.data.stickied && newPost.kind === 't3' && urlMatch) {
+
+		// Grab the author, title, and URL 
+		author = newPost.data.author
+		title = newPost.data.title
+		url = newPost.data.url
+		
+		// Download the new post
+        console.log('New post detected')
+		request(newPost.data.url).pipe(fs.createWriteStream("./pics/newestPost.jpg"))
+	}
+})
+.on('error', console.error)
+
+// event listener to watch for new files in pics dir
+chokidar.watch('./pics/newestPost.jpg', {
+	ignored: /(^|[\/\\])\../,
+	usepolling: true,
+	interval: 10000,
+	awaitWriteFinish: {
+		stabilityThreshold: 60000,
+		pollInterval: 10000
+	}}).on('change', (event, path) => {
+	console.log(event);
+
+	// read the media
+	var b64content = fs.readFileSync('./pics/newestPost.jpg', { encoding: 'base64' })
+
+	// first we must post the media to Twitter
+	twit.post('media/upload', { media_data: b64content }, function (err, data, response) {
+		// now we can assign alt text to the media, for use by screen readers and
+		// other text-based presentations and interpreters
+		var mediaIdStr = data.media_id_string
+		var altText = "New Amoled background by: /u/" + author + " titled: " + title + '. Fullsize image can be found here: ' + url
+		var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+	
+		twit.post('media/metadata/create', meta_params, function (err, data, response) {
+		if (!err) {
+			// now we can reference the media and post a tweet (media will attach to the tweet)
+			var params = { status: 'New Post From /r/AmoledBackrounds by: /u/' +
+			 author + ' titled: "' + title + '". Fullsize image can be found here: ' + url, media_ids: [mediaIdStr] }
+	
+			twit.post('statuses/update', params, function (err, data, response) {
+			console.log(data)
+			})
+		} else {
+			console.log(err)
+		}
+	})
+})
+});
